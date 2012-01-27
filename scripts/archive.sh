0116bc2ab49d28cf3f2fc84cdb6c6cd82730ea55
Adam Hubscher <offbeatadam@gmail.com>
2012-01-27 02:05:10 -0600

    I will fix this damn trailing whitespace I will!

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -20,9 +20,9 @@ do
     printf "Current Working Directory: %s/%s/%s\n" "$LOGDIR" "$i" "$j"
     cd $LOGDIR/$i/$j
   
-    for k in `find . -type f -atime +7`
+    for k in `find . -type f -atime +7 | cut -b 3-`
     do
-      printf "Archiving File %s/%s/%s/%s to %s/%s/%s/%s" "$LOGDIR" "$i" $j" "$k" "$ARCHIVE" "$i" $j" "$k"
+      printf "Archiving File %s/%s/%s/%s to %s/%s/%s/%s\n" "$LOGDIR" "$i" $j" "$k" "$ARCHIVE" "$i" $j" "$k"
       cd $LOGDIR; 
     done
   done
